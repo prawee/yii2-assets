afaabf8156a93c758c5de6206448dfe399108491
Prawee Wongsa <prawee@hotmail.com>
2014-11-07 09:44:10 +0700

    first commit.

---
+++ PwAsset.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * 2014-11-07
+ * @author Prawee Wongsa <konkeanweb@gmail.com>
+ * 
+ */
+namespace prawee\assets;
+
+use yii\web\AssetBundle;
+class PwAsset extends AssetBundle
+{
+    /*
+     * @inheritdoc
+     */
+    public $sourcePath='@prawee/assets';
+
+    /*
+     * @inheritdoc
+     */
+    public $css=[
+    ];
+
+    /*
+     * @inheritdoc
+     */
+    public $js=[
+    ];
+
+    /*
+     * @inheritdoc
+     */
+    public $depends=[
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2 Assets
+===========
+Management Asset for Yii 2.0 Application
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist prawee/yii2-assets "*"
+```
+
+or add
+
+```
+"prawee/yii2-assets": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \prawee\assets\AutoloadExample::widget(); ?>```
